d08c717564f299b93561d9302ab53711a0140168
Denis Bukashkin <dbukashkin@nes.ru>
2019-09-10 16:36:50 +0300

    Third comment

--- Lec2.py
+++ Lec2.py
@@ -31,3 +31,27 @@ print (1<2)
 
 #%%
 
+print (int(False), bool (0))
+
+#%%
+
+
+a = 2
+b = 3
+c = 1.32e+20
+print (a ,b,c )
+
+#%%
+a = 4
+b = 4
+print (a**b)
+a = 2
+#%%
+a = 1.0e-11
+b = 2.435e-13
+from numpy import abs  
+
+print (abs(a-b)<1.e-9)
+
+
+
