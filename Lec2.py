b6c5db36d57d91f811523491362fc6ef67017163
Denis Bukashkin <dbukashkin@nes.ru>
2019-09-10 16:19:24 +0300

    Second comment

---
+++ Lec2.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+
+a = 1+52+\
+    2 + 2
+
+print (a)
+
+#%%
+
+
+print (18)
+#%%
+a = True
+
+b = False
+
+print (a,b)
+
+#%%
+
+print (1>2)
+print (1<2)
+
+
+#%%
+
